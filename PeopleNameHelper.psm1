b4d02474ee3b0f7486ef2cd77c03574fef55438a
stilltorn <am.stilltorn@gmail.com>
2020-11-13 19:36:03 +0200

    Intended to contain functions to help parse names from lists etc

--- PeopleNameHelper.psm1
+++ PeopleNameHelper.psm1
@@ -26,10 +26,18 @@ function Find-SurAndGivenName
         if ($bothNames -contains $word) {
             # NOTE Sometimes a name can of course be both a surname and a givenname, 
             # such as the finnish name Lauri. The following if - ifelse statement 
-            # gives preference for Lauri to be either a given name.
+            # gives preference for Lauri to be a given name.
             # TODO Add more advanced logic so maybe if a word could be either or, 
-            # check if the second one is only on the list of  accepted surnames. Then we 
+            # check if the second one is only on the list of accepted surnames. Then we 
             # can be fairly sure the first word is a givenname
+            # TODO Should probably pick out all names in a continuous line (say 5 words 
+            # next to each other all are names) first, then decide what to do wit all 
+            # of them. Right now the script will not handle 
+            # that sort of thing well. For example:
+            # Givenname givenname givenname givenname - probably just a list of given names
+            # of four different people
+            # Givenname givenname surname - the givenname in between is probably a middle name, 
+            # not to mention it could be a single letter initial of a middle name
             if ($givenNames -contains $word) {
                 $nameObj.GivenName = $word
             } elseif ($surNames -contains $word) {
